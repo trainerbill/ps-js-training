4018825bf461e701dc011e6b54ab514fa905f77b
Andrew Throener <athroener@mystartupsolutions.com>
2017-03-09 00:21:46 -0600

    Update

---
+++ src/routes/paypal/classic/setexpresscheckout/ex1.js
@@ -0,0 +1,23 @@
+export default function (router) {
+    router.get('/', (req, res) => {
+        
+        let paypal = req.app.kraken.get('paypalClassic');
+        let c9config = req.app.kraken.get('cloud9');
+
+        let nvpParams = {
+            RETURNURL: 'https://' + c9config.workspace + '-' + c9config.username + '.c9users.io/misc/returnurl.html',
+            CANCELURL: 'https://' + c9config.workspace + '-' + c9config.username + '.c9users.io/misc/cancelurl.html',
+            PAYMENTREQUEST_0_AMT: '100.00'
+        };
+            
+        paypal.call('SetExpressCheckout', nvpParams, (err, response) => {
+            if (err) {
+                res.json(err);
+            } else {
+                response.TOKEN = { TOKEN: response.TOKEN };
+                res.json(response);
+            }
+
+        });
+    });
+}
